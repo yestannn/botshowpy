5267d57ba5c308ea88d30d30fd8291b531a85bc6
yestannn <55324492+yestannn@users.noreply.github.com>
2020-02-14 15:44:45 +0600

    Update googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -1,11 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
 
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", 
+         "https://www.googleapis.com/auth/drive"]
+
 creds = ServiceAccountCredentials.from_json_keyfile_name("sheet.json", scope)
+
 client = gspread.authorize(creds)
+
 sheet = client.open("pythonsheet").sheet1
 
 
